60911da76a4e57b76b7025b91343545dadd11751
Riyad Parvez <social.riyad@gmail.com>
2018-05-17 02:27:59 -0400

    Added more util methods

--- utils/pysparkutils.py
+++ utils/pysparkutils.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from pyspark.ml.feature import StringIndexer
 from pyspark.sql.functions import col, countDistinct, lit
 
@@ -5,6 +6,37 @@ import math
 import numpy as np
 import pandas as pd
 import scipy.stats
+
+import collections
+
+class FrozenDict(collections.Mapping):
+    """Don't forget the docstrings!!"""
+
+    def __init__(self, *args, **kwargs):
+        self._d = dict(*args, **kwargs)
+        self._hash = None
+
+    def __iter__(self):
+        return iter(self._d)
+
+    def __len__(self):
+        return len(self._d)
+
+    def __getitem__(self, key):
+        return self._d[key]
+
+    def __hash__(self):
+        # It would have been simpler and maybe more obvious to
+        # use hash(tuple(sorted(self._d.items()))) from this discussion
+        # so far, but this solution is O(n). I don't know what kind of
+        # n we are going to run into, but sometimes it's hard to resist the
+        # urge to optimize when it will gain improved algorithmic performance.
+        if self._hash is None:
+            self._hash = 0
+            for pair in self.items():
+                self._hash ^= hash(pair)
+        return self._hash
+
 
 def findMissingValuesCols(df):
     nullCols = []
@@ -59,7 +91,8 @@ def calcIndividualAndJointPorbablities(df, *columns):
         rows = aggr.withColumn('prob', (col('count') / n)).collect()
         for row in rows:
             val = row[column]
-            individualProbs[(column, val, )] = row['prob']
+            key = (column, val)
+            individualProbs[key] = row['prob']
             columnDistinctVals[column].append(val)
 
     aggr = df.groupby(*columns).count()
@@ -96,6 +129,9 @@ def calcPointwiseMutualInformation(df, *columns):
         pmi[k] = g
     return pmi
 
+# no co-occurrences, logp(x,y)→−∞, so nmpi is -1,
+# co-occurrences at random, logp(x,y)=log[p(x)p(y)], so nmpi is 0,
+# complete co-occurrences, logp(x,y)=logp(x)=logp(y), so nmpi is 1.
 def calcNormalizedPointwiseMutualInformation(df, *columns):
     individualProbs, jointProbs = calcIndividualAndJointPorbablities(df, *columns)
     npmi = {}
@@ -103,7 +139,7 @@ def calcNormalizedPointwiseMutualInformation(df, *columns):
         jointProb = v
         indProbs = [individualProbs[ind] for ind in k]
         pmi = math.log(jointProb / reduce((lambda x, y: x * y), indProbs))
-        npmi[k] = pmi / math.log(jointProb)
+        npmi[k] = pmi / -math.log(jointProb)
     return npmi
 
 def calcNormalizedMutualInformation(df, col1, col2):
@@ -115,3 +151,6 @@ def stratifiedSampling(df, key, fraction):
     first = df.sampleBy(key, fractions, 42)
     second = df.subtract(first)
     return first, second
+
+def dictToPandasDF(dictionary, *columns):
+    return pd.DataFrame(list(dictionary.items()), columns=[*columns])
